a3f6bb30053224411e052b10cdb2edac40afebe9
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-06-23 11:08:13 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -6,9 +6,13 @@ int alertFailureCount = 0;
 int networkAlertStub(float celcius) {
     std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
     // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
+    // Return 500 for not-ok    
+    if(celcius > 200) {
+        return 500;
+    }
+    else {
+        return 200;
+    }
 }
 
 void alertInCelcius(float farenheit) {
@@ -26,6 +30,7 @@ void alertInCelcius(float farenheit) {
 int main() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
+    assert(alertFailureCount == 1);
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
