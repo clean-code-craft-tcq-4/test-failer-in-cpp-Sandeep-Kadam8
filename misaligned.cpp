7be524faf397e2226450cb2a25fd8ec293c0daab
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-09-22 10:17:45 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -4,18 +4,6 @@
 #include <vector>
 #include <iomanip>
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-        }
-    }
-    return i * j;
-}
-
 struct colorPairLineItem {
     colorPairLineItem(int index_, const std::string &majorColor_, const std::string &minorColor_)
     : index(index_), majorColor(majorColor_), minorColor(minorColor_)
@@ -31,7 +19,7 @@ std::vector<colorPairLineItem> constructColorMap(const char *majorColor[], size_
     std::vector<colorPairLineItem> list;
     for(size_t i = 0; i < majorColorSize; i++) {
         for(size_t j = 0; j < minorColorSize; j++) {
-            list.push_back(colorPairLineItem(i * 5 + j, majorColor[i], minorColor[i]));
+            list.push_back(colorPairLineItem(i * 5 + (j + 1), majorColor[i], minorColor[i]));
         }
     }
     return list;
@@ -39,12 +27,25 @@ std::vector<colorPairLineItem> constructColorMap(const char *majorColor[], size_
 
 std::string colorPairToString(colorPairLineItem item) {
     std::stringstream colorPairItem;
-    colorPairItem << item.index << " | " << item.majorColor << " | " << item.minorColor << "\n";
+    colorPairItem << std::setw(2) << item.index << " | "
+        << std::setw(6) << item.majorColor << " | "
+        << std::setw(6) << item.minorColor << " | "
+        << "\n";
     return colorPairItem.str();
 }
 
 void printData(const std::string &data) {
     std::cout << data;
+}
+
+int printColorMap(const std::vector<colorPairLineItem> &list) {
+    int count = 0;
+    for(const auto &item:list) {
+        printData(colorPairToString(item));
+        count++;
+    }
+
+   return count;
 }
 
 int main() {
@@ -57,13 +58,13 @@ int main() {
 
     assert(listItem.index == 1);
     std::stringstream str;
-    str << std::setw(2) << listItem.index
-        << std::setw(6) << listItem.majorColor
-        << std::setw(6) << listItem.minorColor
+    str << std::setw(2) << listItem.index << " | "
+        << std::setw(6) << listItem.majorColor << " | "
+        << std::setw(6) << listItem.minorColor << " | "
         << "\n";
     assert(colorPairToString(listItem) == str.str());
 
-    int result = printColorMap();
+    int result = printColorMap(list);
     assert(result == 25);
     std::cout << "All is well (maybe!)\n";
     return 0;
