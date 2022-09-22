826924712b4a18013c7deb5066144100412117a8
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-09-22 09:40:40 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
 #include <assert.h>
+#include <functional>
+#include <vector>
+
+bool isSmall(int cms, char &sizeName) {
+    if(cms < 38) {
+        sizeName = 'S';
+        return true;
+    }
+    return false;
+}
+
+bool isMedium(int cms, char &sizeName) {
+    if(cms >= 38 && cms < 42) {
+        sizeName = 'M';
+        return true;
+    }
+    return false;
+}
+
+bool isLarge(int cms, char &sizeName) {
+    if(cms >= 42 && cms < 46) {
+        sizeName = 'L';
+        return true;
+    }
+    return false;
+}
+
+std::vector<std::function<bool(int, char &)>> sizeCheckList = {isSmall, isMedium, isLarge};
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
-        sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
-        sizeName = 'M';
-    } else if(cms > 42) {
-        sizeName = 'L';
+    for(auto &itr: sizeCheckList) {
+        if(itr(cms, sizeName)) {
+            break;
+        }
     }
     return sizeName;
 }
@@ -18,7 +44,8 @@ int main() {
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     assert(size(38) == 'M');
-    assert(size(42) == 'M');
+    assert(size(42) == 'L');
+    assert(size(50) == '\0');
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
